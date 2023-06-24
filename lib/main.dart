181da66fa7c7bcc1c553849e9287e2b8450bc448
hongwen000 <hongwen000@outlook.com>
2023-06-24 13:08:29 +0800

    custom font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_chatgpt/controller/auth.dart';
 import 'package:flutter_chatgpt/controller/conversation.dart';
 import 'package:flutter_chatgpt/controller/message.dart';
@@ -11,10 +15,33 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_chatgpt/route.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart';
+
+Future<ByteData> fetchFont() async {
+  var url = Uri.parse(
+      'https://gstatic.loli.net/s/sourcecodepro/v6/mrl8jkM18OlOQN8JLgasD9Rl0pGnog23EMYRrBmUzJQ.ttf');
+  final response = await get(url,
+      headers: <String, String>{
+        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+  );
+
+  if (response.statusCode == 200) {
+    return ByteData.view(response.bodyBytes.buffer);
+  } else {
+    throw Exception('Failed to load font');
+  }
+}
 
 void main() async {
   await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb) {
+    var fontLoader = FontLoader("wqy");
+          fontLoader.addFont(fetchFont());
+          await fontLoader.load();
+  }
   runApp(const MyApp());
 }
 
@@ -28,18 +55,34 @@ class MyApp extends StatelessWidget {
     Get.put(MessageController());
     Get.put(PromptController());
     Get.put(AuthController()); // Add this line
-    return GetMaterialApp(
-      initialRoute: '/',
-      getPages: routes,
-      unknownRoute:
-          GetPage(name: '/not_found', page: () => const UnknownRoutePage()),
-      theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay),
-      darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay),
-      themeMode: ThemeMode.system,
-      locale: const Locale('zh'),
-      translations: MyTranslations(),
-      builder: EasyLoading.init(),
-      debugShowCheckedModeBanner: false,
-    );
+    if (kIsWeb) {
+      return GetMaterialApp(
+        initialRoute: '/',
+        getPages: routes,
+        unknownRoute:
+            GetPage(name: '/not_found', page: () => const UnknownRoutePage()),
+        theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay, fontFamily: "wqy"),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay, fontFamily: "wqy"),
+        themeMode: ThemeMode.system,
+        locale: const Locale('zh'),
+        translations: MyTranslations(),
+        builder: EasyLoading.init(),
+        debugShowCheckedModeBanner: false,
+      );
+    } else {
+      return GetMaterialApp(
+        initialRoute: '/',
+        getPages: routes,
+        unknownRoute:
+            GetPage(name: '/not_found', page: () => const UnknownRoutePage()),
+        theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay),
+        themeMode: ThemeMode.system,
+        locale: const Locale('zh'),
+        translations: MyTranslations(),
+        builder: EasyLoading.init(),
+        debugShowCheckedModeBanner: false,
+      );
+    }
   }
 }
